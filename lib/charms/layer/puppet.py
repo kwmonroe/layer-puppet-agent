603bd349281a9f2898f2a33597d7e087cbe814ed
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2016-05-30 13:45:20 +0300

    Lint fixes

--- lib/charms/layer/puppet.py
+++ lib/charms/layer/puppet.py
@@ -24,7 +24,7 @@ class PuppetConfigs:
         self.version = config['puppet-version']
         self.puppet_base_url = 'https://apt.puppetlabs.com'
         self.puppet_conf = 'puppet.conf'
-        self.auto_start = ('yes','no')
+        self.auto_start = ('yes', 'no')
         self.ensure_running = 'false'
         self.ubuntu_release = lsb_release()['DISTRIB_CODENAME']
         self.puppet_ssl_dir = '/var/lib/puppet/ssl/'
@@ -32,7 +32,7 @@ class PuppetConfigs:
 
         if config['puppet-version'] == 4:
             self.puppet_pkgs = ['puppet-agent']
-            self.puppet_purge = ['puppet','puppet-common']
+            self.puppet_purge = ['puppet', 'puppet-common']
             if config['pin-puppet']:
                 self.puppet_pkg_vers = \
                     [('puppet-agent=%s' % config['pin-puppet'])]
@@ -54,7 +54,7 @@ class PuppetConfigs:
             if config['pin-puppet']:
                 self.puppet_pkg_vers = \
                     [('puppet=%s' % config['pin-puppet']),
-                    ('puppet-common=%s' % config['pin-puppet'])]
+                     ('puppet-common=%s' % config['pin-puppet'])]
             else:
                 self.puppet_pkg_vers = self.puppet_pkgs
             self.puppet_deb = 'puppetlabs-release-%s.deb' % \
@@ -62,7 +62,7 @@ class PuppetConfigs:
             self.puppet_exe = '/usr/bin/puppet'
             self.puppet_conf_dir = '/etc/puppet'
             if config['auto-start']:
-                self.auto_start = ('no','yes')
+                self.auto_start = ('no', 'yes')
             self.enable_puppet_cmd = \
                 ('sed -i /etc/default/puppet '
                  '-e s/START=%s/START=%s/' % self.auto_start)
@@ -76,7 +76,6 @@ class PuppetConfigs:
         if config['ca-server']:
             self.puppet_conf_ctxt['ca_server'] = config['ca-server']
 
-
     def render_puppet_conf(self):
         ''' Render puppet.conf
         '''
@@ -88,13 +87,11 @@ class PuppetConfigs:
                perms=0o644,
                context=self.puppet_conf_ctxt)
 
-
     def puppet_conf_path(self):
         '''Return fully qualified path to puppet.conf
         '''
         puppet_conf_path = '%s/%s' % (self.puppet_conf_dir, self.puppet_conf)
         return puppet_conf_path
-
 
     def puppet_deb_url(self):
         '''Return fully qualified puppet deb url
@@ -102,67 +99,56 @@ class PuppetConfigs:
         puppet_deb_url = '%s/%s' % (self.puppet_base_url, self.puppet_deb)
         return puppet_deb_url
 
-
     def puppet_deb_temp(self):
         '''Return fully qualified path to downloaded deb
         '''
         puppet_deb_temp = os.path.join('/tmp', self.puppet_deb)
         return puppet_deb_temp
 
-
     def puppet_running(self):
-    
+
         '''Enable or disable puppet auto-start
         '''
         call(self.enable_puppet_cmd.split(), shell=False)
 
-
-#class Puppet(object):
-
     def puppet_active(self):
         if config['auto-start']:
-            hookenv.status_set('active', 
+            hookenv.status_set('active',
                                'Puppet-agent running')
         else:
-            hookenv.status_set('active', 
+            hookenv.status_set('active',
                                'Puppet-agent installed, but not running')
-    
-    
+
     def fetch_install_puppet_deb(self, puppet):
         '''Fetch and install the puppet deb
         '''
-        hookenv.status_set('maintenance', 
+        hookenv.status_set('maintenance',
                            'Configuring Puppetlabs apt sources')
         aufh = ArchiveUrlFetchHandler()
         aufh.download(self.puppet_deb_url(), self.puppet_deb_temp())
         dpkg_puppet_deb = 'dpkg -i %s' % self.puppet_deb_temp()
         call(dpkg_puppet_deb.split(), shell=False)
         apt_update()
-    
+
         # Clean up
         rm_trusty_puppet_deb = 'rm %s' % self.puppet_deb_temp()
         call(rm_trusty_puppet_deb.split(), shell=False)
         self.puppet_active()
-    
-    
+
     def install_puppet(self):
-    
         '''Install puppet
         '''
-        hookenv.status_set('maintenance', 
+        hookenv.status_set('maintenance',
                            'Installing puppet agent')
         self.fetch_install_puppet_deb(self)
         apt_install(self.puppet_pkg_vers)
         apt_hold(self.puppet_pkgs)
-    
-    
+
     def configure_puppet(self):
-    
         '''Configure puppet
         '''
-        hookenv.status_set('maintenance', 
+        hookenv.status_set('maintenance',
                            'Configuring puppet agent')
         self.render_puppet_conf()
         self.puppet_running()
         self.puppet_active()
-    
